6e7ca7587715eb2d7f23474a3f3a07043dc2adad
Paul Goins <general@vultaire.net>
2013-11-18 23:55:28 -0800

    Initial commit

---
+++ gen/R.java.d
@@ -0,0 +1,12 @@
+C:\Users\Vultaire\code\android\soundboard\gen\net\vultaire\soundboard\R.java \
+ : C:\Users\Vultaire\code\android\soundboard\res\drawable-hdpi\ic_launcher.png \
+C:\Users\Vultaire\code\android\soundboard\res\drawable-ldpi\ic_launcher.png \
+C:\Users\Vultaire\code\android\soundboard\res\drawable-mdpi\ic_launcher.png \
+C:\Users\Vultaire\code\android\soundboard\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\Vultaire\code\android\soundboard\res\layout\main.xml \
+C:\Users\Vultaire\code\android\soundboard\res\values\strings.xml \
+C:\Users\Vultaire\code\android\soundboard\bin\res\drawable-hdpi\ic_launcher.png \
+C:\Users\Vultaire\code\android\soundboard\bin\res\drawable-ldpi\ic_launcher.png \
+C:\Users\Vultaire\code\android\soundboard\bin\res\drawable-mdpi\ic_launcher.png \
+C:\Users\Vultaire\code\android\soundboard\bin\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\Vultaire\code\android\soundboard\bin\AndroidManifest.xml \
