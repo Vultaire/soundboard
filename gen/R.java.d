e400c34fb204d284a81ae935a5a6682f8f98438a
Paul Goins <general@vultaire.net>
2013-11-19 23:20:26 -0800

    Added play/stop buttons and a sample button in the layout.

--- gen/R.java.d
+++ gen/R.java.d
@@ -1,11 +1,15 @@
 C:\Users\Vultaire\code\android\soundboard\gen\net\vultaire\soundboard\R.java \
- : C:\Users\Vultaire\code\android\soundboard\res\drawable-hdpi\ic_launcher.png \
+ : C:\Users\Vultaire\code\android\soundboard\res\drawable\play.png \
+C:\Users\Vultaire\code\android\soundboard\res\drawable\stop.png \
+C:\Users\Vultaire\code\android\soundboard\res\drawable-hdpi\ic_launcher.png \
 C:\Users\Vultaire\code\android\soundboard\res\drawable-ldpi\ic_launcher.png \
 C:\Users\Vultaire\code\android\soundboard\res\drawable-mdpi\ic_launcher.png \
 C:\Users\Vultaire\code\android\soundboard\res\drawable-xhdpi\ic_launcher.png \
 C:\Users\Vultaire\code\android\soundboard\res\layout\main.xml \
 C:\Users\Vultaire\code\android\soundboard\res\raw\trololo.mp3 \
 C:\Users\Vultaire\code\android\soundboard\res\values\strings.xml \
+C:\Users\Vultaire\code\android\soundboard\bin\res\drawable\play.png \
+C:\Users\Vultaire\code\android\soundboard\bin\res\drawable\stop.png \
 C:\Users\Vultaire\code\android\soundboard\bin\res\drawable-hdpi\ic_launcher.png \
 C:\Users\Vultaire\code\android\soundboard\bin\res\drawable-ldpi\ic_launcher.png \
 C:\Users\Vultaire\code\android\soundboard\bin\res\drawable-mdpi\ic_launcher.png \
