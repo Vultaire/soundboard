9c007b83675288a48a3aadce3e5752cca37fb136
Paul Goins <general@vultaire.net>
2013-11-21 01:45:59 -0800

    Reworked layout to use a GridView with Buttons.  Created some mock data to drive the GridView for now.

--- src/net/vultaire/soundboard/Soundboard.java
+++ src/net/vultaire/soundboard/Soundboard.java
@@ -6,12 +6,42 @@ import android.media.AudioManager;
 import android.media.AudioManager.OnAudioFocusChangeListener;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
+import android.widget.GridView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
 
 public class Soundboard extends Activity
 {
-    private AudioManager am = null;
-    private OnAudioFocusChangeListener afChangeListener = null;
-    private MediaPlayer mp = null;
+    private AudioManager am;
+    private OnAudioFocusChangeListener afChangeListener;
+    private MediaPlayer mp;
+
+    class ButtonArrayAdapter extends ArrayAdapter<String> {
+	Context context;
+	int resource;
+	String[] objects;
+	ButtonArrayAdapter(Context context, int resource, String[] objects) {
+	    super(context, resource, objects);
+	    this.context = context;
+	    this.resource = resource;
+	    this.objects = objects;
+	}
+	public View getView(int position, View convertView, ViewGroup parent) {
+	    if (convertView == null) {
+		LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+		/* Not *completely* sure on this command.  Should I
+		 * use parent?  Should I use null?  Not fully
+		 * understanding the difference yet. */
+		convertView = inflater.inflate(resource, null);
+	    }
+	    ((Button)convertView).setText(objects[position]);
+	    return convertView;
+	}
+    }
 
     /** Called when the activity is first created. */
     @Override
@@ -24,6 +54,10 @@ public class Soundboard extends Activity
 	afChangeListener = new OnAudioFocusChangeListener() {
 	    public void onAudioFocusChange(int focusChange) {}
 	};
+	String[] data = {"Trololo", "Shut up, Beavis!"};
+	ArrayAdapter adapter = new ButtonArrayAdapter(this, R.layout.button, data);
+	GridView gridView = (GridView) findViewById(R.id.grid_view);
+	gridView.setAdapter(adapter);
     }
 
     @Override
